d43dd228fb1b856ff170f7729d15252aa51b67f6
SF\89010729 <89010729@sfmail.sf-express.com>
2020-07-28 16:57:56 +0800

    将测试陷害慕上传到git是否成功

---
+++ my_gadget/src/test/java/com/hjx/OauthRbacRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjx;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthRbacRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
